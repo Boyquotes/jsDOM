90392c3e026fe5001711107327f6c69ae2efb012
Boyquotes <webmaster@linux-live-cd.org>
2015-11-30 14:28:11 +0100

    Init

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+	var texte = document.getElementById('monTexte').innerHTML;
+console.log(texte);
+
+	var longueur = texte.length;
+console.log(longueur);
+
+	if(longueur > 100){
+console.log('trop long');
+	}
