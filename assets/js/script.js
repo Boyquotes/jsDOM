3784109c058091ab8f6271e2af948ba026e4daa9
Boyquotes <webmaster@linux-live-cd.org>
2015-11-30 14:35:28 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,1 @@
-	var texte = document.getElementById('monTexte').innerHTML;
-console.log(texte);
 
-	var longueur = texte.length;
-console.log(longueur);
-
-	if(longueur > 100){
-console.log('trop long');
-	}
