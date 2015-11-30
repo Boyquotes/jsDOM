ea4e4864e80d84994149b81f7d2e7c7c3626620f
Boyquotes <webmaster@linux-live-cd.org>
2015-11-30 16:17:04 +0100

    Ajout de l'ID sur la liste d element

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,42 @@ console.log(texte);
 	var longueur = texte.length;
 console.log(longueur);
 
+	var texte = document.getElementById('monTexte2').innerHTML;
+console.log(texte);
+
+	var longueur = texte.length;
+console.log(longueur);
+
+	var texte = document.getElementById('texte').innerHTML;
+console.log(texte);
+
+	var longueur = texte.length;
+console.log(longueur);
+
+var divs = document.getElementsByTagName('div');
+c = divs.length; 
+for (var i = 0; i < c ; i++) {
+	var texte = divs[i].innerHTML;
+console.log(texte);
+	var longueur = texte.length;
+console.log(longueur);
+
 	if(longueur > 100){
 console.log('trop long');
+		texte = texte.substr(0, 100);
+		texte += "...";
+console.log(texte);
+		divs[i].innerHTML = texte;
 	}
+//    alert('Element n° ' + (i + 1) + ' : ' + divs[i]);
+}
+
+var ul = document.ul.getElementsByTagName('li');
+
+console.log(ul);
+var c = ul.lenght;
+alert(c);
+for (var i=0;i < c; i++){
+	console.log('un li');
+}
+
